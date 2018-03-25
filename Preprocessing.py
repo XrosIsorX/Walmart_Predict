8a271b47c8a868ae8627f616e0aac9343baf5486
olan <olantoj@hotmail.com>
2018-03-25 20:24:04 +0700

    split one hot coding

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from keras.utils import to_categorical
 
 features = pd.read_csv("data/features.csv")
 test = pd.read_csv("data/test.csv")
@@ -7,9 +8,14 @@ stores = pd.read_csv("data/stores.csv")
 
 merged_train = pd.merge(train, stores)
 merged_train = pd.merge(merged_train , features)
+merged_train = merged_train.fillna(0.)
+merged_train = merged_train.drop('Date', 1)
+merged_train = pd.get_dummies(merged_train, columns=['Store', 'Dept', 'Type'])
+merged_train.to_csv("data/merged_train.csv", index =False)
 
 merged_test = pd.merge(test, stores)
-merged_test = pd.merge(merged_train, features)
-
-merged_train.to_csv("data/merged_train.csv")
-merged_test.to_csv("data/merged_test.csv")
+merged_test = pd.merge(merged_test, features)
+merged_test = merged_test.fillna(0.)
+merged_test = merged_test.drop('Date', 1)
+merged_test = pd.get_dummies(merged_test, columns=['Store', 'Dept', 'Type'])
+merged_test.to_csv("data/merged_train.csv", index =False)
---
+++ create_models.py
@@ -0,0 +1,36 @@
+import numpy as np
+from keras.utils import to_categorical
+import matplotlib as plt
+import pandas as pd
+
+data = pd.read_csv("data/merged_train.csv")
+train_x = data.drop('Weekly_Sales', 1)
+train_y = data['Weekly_Sales']
+
+from sklearn.model_selection import train_test_split
+train_x, valid_x, train_y, valid_y = train_test_split(train_x, train_y, test_size=0.2, random_state=14)
+
+import keras 
+from keras.models import Sequential, Input, Model
+from keras.layers import Dense, Dropout
+from keras.layers.normalization import BatchNormalization
+from keras.layers.advanced_activations import LeakyReLU
+
+batch_size = 32
+epochs = 100
+
+#Add network
+model = Sequential()
+model.add(Dense(10, activation='linear'))
+model.add(LeakyReLU(alpha=0.0))
+model.add(Dense(5, activation='linear'))
+model.add(LeakyReLU(alpha=0.0))
+model.add(Dense(1, activation='linear'))
+
+model.compile(loss=keras.losses.mean_squared_error, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+
+model.summary()
+
+#Train network
+model_train = model.fit(train_x, train_y, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(valid_x, valid_y))
+model.save("fashion_model.h5py")
